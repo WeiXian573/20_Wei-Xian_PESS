ab26bad59c624c8aa588f09bb8ea48f8ed7e2a14
WeiXian573 <92509469+WeiXian573@users.noreply.github.com>
2021-10-15 17:20:15 +0800

    Add files via upload

--- nav.php
+++ nav.php
@@ -1,7 +1,7 @@
-<h1>Police Emergency Service System</h1>
-<nav>
+<div id="banner"><img src="banner.jpg"></div>
+<div class="topnav">
  <a href="logcall.php">Log Call</a>
- <a href="Update.php">Update</a>
+ <a href="update.php">Update</a>
  <a href="#">Report</a>
  <a href="#">History</a>
- </nav>
+ </div>
